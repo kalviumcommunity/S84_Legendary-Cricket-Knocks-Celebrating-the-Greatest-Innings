8d3fc25add5b4f9580266370d354ca2ed7b43f7f
Ashish <iashishyadav17@gmai.com>
2025-06-24 23:32:06 +0530

    temp rename to fix case sensitivity

--- frontends/src/pages/home.jsx
+++ frontends/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import KnockCard from "../components/KnockCard";
+import KnockCard from "../components/knockCard";
 
 const API_BASE_URL = "http://localhost:3000";
 
